206b7894c14e802e75472a2cc870131519fc9d0d
TopsyCretts <topsycrettschannel@gmail.com>
2023-10-31 23:23:23 +0300

    learned fragments and audio started to use it in project

---
+++ app/src/main/java/com/example/trainandroid/KissCatFrag.java
@@ -0,0 +1,34 @@
+package com.example.trainandroid;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class KissCatFrag extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_kiss_cat, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button start, loop;
+        start = view.findViewById(R.id.start_mrr);
+        loop = view.findViewById(R.id.lopp_murr);
+
+
+    }
+}
--- app/src/main/java/com/example/trainandroid/MainActivity.java
+++ app/src/main/java/com/example/trainandroid/MainActivity.java
@@ -2,25 +2,25 @@ package com.example.trainandroid;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText number1;
-    EditText number2;
-    TextView result;
-    Button addButton;
-    Button showButton;
-    ImageView image;
-    Button like_button;
+    private Button showButton;
+    private ImageView image;
+    private Button like_button;
+    private FrameLayout frameLayout;
+
 
 
     @Override
@@ -28,13 +28,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        number1 = findViewById(R.id.number1);
-        number2 = findViewById(R.id.number2);
-        result = findViewById(R.id.result_View);
-        addButton = findViewById(R.id.addButton);
         showButton = findViewById(R.id.show);
         image = findViewById(R.id.tanya);
         like_button = findViewById(R.id.like_button);
+        frameLayout = findViewById(R.id.frameLayout);
 
         showButton.setOnClickListener(new View.OnClickListener() {
             boolean showMode = false;
@@ -53,24 +50,13 @@ public class MainActivity extends AppCompatActivity {
                         }
         }
     });
-        addButton.setOnClickListener(v -> {
-            if (number1.getText().toString().isEmpty()||number2.getText().toString().isEmpty()){
-                Toast.makeText(MainActivity.this, "Введите числа",Toast.LENGTH_SHORT).show();
-                return;
-            }
-            float numberOne = Float.parseFloat(number1.getText().toString());
-            float numberTwo = Float.parseFloat(number2.getText().toString());
-            float resultNumber = numberTwo+numberOne;
-            result.setText(String.valueOf(resultNumber));
-            Toast.makeText(MainActivity.this, "Результат: "+resultNumber, Toast.LENGTH_SHORT).show();
-        });
-        like_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showInfo("А что ты выберешь, а?","Погладишь киску?))", "Канешна",
-                        "Ты хороший кожаный", "Нихотю чёта", "Ну и нинада..." );
-            }
-        });
+        like_button.setOnClickListener(v -> showInfo("А что ты выберешь, а?","Погладишь киску?))", "Канешна",
+                "Ты хороший кожаный", "Нихотю чёта", "Ну и нинада..." ));
+        KissCatFrag kissCatFrag = new KissCatFrag();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(), kissCatFrag);
+        fragmentTransaction.commit();
+
     }
     private void showInfo(String title,String message, String positive, String posCon,
                           String negative,String negCon){
@@ -80,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             builder.setPositiveButton(positive, (dialog, which) -> Toast.makeText(MainActivity.this,posCon,Toast.LENGTH_LONG).show());
             builder.setNegativeButton(negative, (dialog, which) -> {
                 Toast.makeText(MainActivity.this, negCon,Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, sad_cat_activity.class);
+                Intent intent = new Intent(MainActivity.this, SadCatAct.class);
                 startActivity(intent);
             });
                     AlertDialog dialog = builder.create();
